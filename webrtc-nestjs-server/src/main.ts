6a5945ed8f9336e59ad85e17a51f289519920d88
Nhan Cao <nhancv92@gmail.com>
2020-03-26 14:17:15 +0700

    remove fastify, add serve-static

--- webrtc-nestjs-server/src/main.ts
+++ webrtc-nestjs-server/src/main.ts
@@ -1,13 +1,11 @@
 import {NestFactory} from '@nestjs/core';
-import {FastifyAdapter, NestFastifyApplication,} from '@nestjs/platform-fastify';
 import {AppModule} from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-      AppModule,
-      new FastifyAdapter()
-  );
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  await app.listen(3000, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
-bootstrap();
+bootstrap().then();

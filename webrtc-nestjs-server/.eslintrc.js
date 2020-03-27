c94590f4f122f6023e969a9d73877fb606dc9fef
Nhan Cao <nhancv92@gmail.com>
2020-03-27 17:23:08 +0700

    add basic webrtc peerconnection

--- webrtc-nestjs-server/.eslintrc.js
+++ webrtc-nestjs-server/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     node: true,
     jest: true,
   },
+  ignorePatterns: ["client/", "node_modules"],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
